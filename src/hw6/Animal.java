96283d74ebbf70c30922508574700c5f9f6c66e7
Andrew <shpuntik0902@gmail.com>
2020-12-11 10:52:02 +0300

    hw6

---
+++ src/hw6/Animal.java
@@ -0,0 +1,33 @@
+package hw6;
+
+public abstract class Animal {
+    protected double runLimit, jumpLimit, swimLimit;
+
+    public Animal() {
+        runLimit = 0;
+        jumpLimit = 0;
+        swimLimit = 0;
+    }
+
+    public Animal(double runLimit, double jumpLimit, double swimLimit) {
+        this.runLimit = runLimit;
+        this.jumpLimit = jumpLimit;
+        this.swimLimit = swimLimit;
+    }
+
+    public void run(double distance) {
+        System.out.println("run(" + distance + "): " + (distance >= 0 && distance <= runLimit ? "true" : "false"));
+    }
+
+    public void jump(double distance) {
+        System.out.println("jump(" + distance + "): " + (distance >= 0 && distance <= jumpLimit ? "true" : "false"));
+    }
+
+    public void swim(double distance) {
+        System.out.println("swim(" + distance + "): " + (distance >= 0 && distance <= swimLimit ? "true" : "false"));
+    }
+
+    public void printAnimalInfo() {
+        System.out.println("Animal " + this.toString() + " runLimit: " + runLimit + "; jumpLimit: " + jumpLimit + "; swimLimit: " + swimLimit);
+    }
+}
---
+++ src/hw6/Cat.java
@@ -0,0 +1,18 @@
+package hw6;
+
+public class Cat extends Animal {
+    public Cat() {  // Обычные коты не умеют плавать
+        super((int) Math.random() * 201 + 100, (int) Math.random() * 2 + 1, 0);
+    }
+
+    public Cat(double runLimit, double jumpLimit, double swimLimit) {
+        super(runLimit, jumpLimit, swimLimit);
+        //если добавится еще какое-то ограничение, то будем его описывать тут + добавим поле для этого ограничения в этом классе
+    }
+
+    @Override
+    public void swim(double distance) {
+        if (swimLimit == 0) System.out.println("Cat can't swim!");  // Обычные коты не умеют плавать
+        else super.swim(distance);
+    }
+}
