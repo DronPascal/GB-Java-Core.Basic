a2a998bf917c6debdf7836b86ba52bd619d9c41c
Andrew <shpuntik0902@gmail.com>
2020-12-17 20:42:49 +0300

    hw8

--- hw8/src/sample/Controller.java
+++ hw8/src/sample/Controller.java
@@ -1,11 +1,14 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
+    GameMaster gameMaster;
 
     @FXML
     private TextField numberTfld;
@@ -26,20 +29,88 @@ public class Controller {
     private Label infoLabelDynamic;
 
     @FXML
-    public void initialize(){
+    public void initialize() {
+        gameMaster = new GameMaster();
         playAgainBtn.setVisible(false);
         infoLabelStatic.setVisible(false);
         infoLabelDynamic.setVisible(false);
+        checkBtn.setDisable(false);
+        numberTfld.setDisable(false);
+        tryCountLabel.setVisible(true);
+        tryCountLabel.setText("Осталось " + fixNumerical(gameMaster.MAX_TRY_COUNT, "попытка", "попытки", "попыток"));
+        numberTfld.clear();
     }
 
     @FXML
     void onCheckBtnClicked() {
-        
+        String inputNumber = numberTfld.getText();
+        if (!inputNumber.isBlank()) {
+            try {
+                int number = Integer.parseInt(inputNumber);
+                if (number < 0 || number > 9) {
+                    new Alert(Alert.AlertType.INFORMATION,
+                            "Введенное значение не входит в интервал от 0 до 9 (включительно).\nПовторите ввод.").show();
+                } else {
+                    gameMaster.increaseAttemptNumber();
+                    if (gameMaster.isGameOver(number)) {
+                        playAgainBtn.setVisible(true);
+                        tryCountLabel.setVisible(true);
+                        infoLabelStatic.setVisible(false);
+                        infoLabelDynamic.setVisible(false);
+                        tryCountLabel.setVisible(false);
+                        checkBtn.setDisable(true);
+                        numberTfld.setDisable(true);
+                        if (gameMaster.isNumberCorrect(number)){
+                            new Alert(Alert.AlertType.INFORMATION,
+                                    "Поздравляем!\nВы угадали, это было число "+ number).show();
+                        }
+                        else if(gameMaster.getAttemptRemaining()==0) {
+                            new Alert(Alert.AlertType.INFORMATION,
+                                    "Вы не успели угадать число!\nЯ загадывал " + gameMaster.getHiddenNumber()).show();
+                        }
+                    }
+                    else {
+                        infoLabelStatic.setVisible(true);
+                        infoLabelDynamic.setVisible(true);
+                        if (number<gameMaster.getHiddenNumber())
+                            infoLabelDynamic.setText("Меньше");
+                        else
+                            infoLabelDynamic.setText("Больше");
+                        tryCountLabel.setText("Осталось " + fixNumerical(gameMaster.getAttemptRemaining(), "попытка", "попытки", "попыток"));
+                        numberTfld.clear();
+                    }
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                new Alert(Alert.AlertType.INFORMATION,
+                        "Введенное значение не является целым числом.\nПовторите ввод.").show();
+            }
+        }
     }
 
     @FXML
     void onPlayAgainBtnClicked() {
-
+        initialize();
     }
 
+    public static String fixNumerical(int num, String... arr) {
+        int preLastDigit = num % 100 / 10;
+        if (preLastDigit == 1) {
+            return String.format("%d %s", num, arr[2]);
+        }
+
+        int lastDigit = num % 10;
+        switch (lastDigit) {
+            case 1:
+                return String.format("%d %s", num, arr[0]);
+            case 2:
+            case 3:
+            case 4:
+                return String.format("%d %s", num, arr[1]);
+            default:
+                return String.format("%d %s", num, arr[2]);
+        }
+    }
 }
+
+
--- hw8/src/sample/GameMaster.java
+++ hw8/src/sample/GameMaster.java
@@ -1,30 +1,34 @@
 package sample;
 
 public class GameMaster {
-    private static final int MAX_TRY_COUNT = 3;
-    private static final int MAX_NUMBER = 9;
+    public static final int MAX_TRY_COUNT = 3;
+    public static final int MAX_NUMBER = 9;
     private int attemptNumber = 1;
-    int randomNumber = (int) (Math.random() * (MAX_NUMBER + 1));
+    int hiddenNumber = (int) (Math.random() * (MAX_NUMBER + 1));
 
     public boolean isNumberCorrect(int number) {
-        return number == randomNumber;
+        return number == hiddenNumber;
     }
 
     public void increaseAttemptNumber(){
         attemptNumber++;
     }
 
-    public boolean isGameOver(){
-        return attemptNumber==MAX_TRY_COUNT;
+    public boolean isGameOver(int inputNumber){
+        return attemptNumber>MAX_TRY_COUNT || inputNumber == hiddenNumber;
     }
 
     public void wipeGameData(){
         attemptNumber=1;
-        randomNumber = (int) (Math.random() * (MAX_NUMBER + 1));
+        hiddenNumber = (int) (Math.random() * (MAX_NUMBER + 1));
     }
 
-    public int getRandomNumber() {
-        return randomNumber;
+    public int getAttemptRemaining(){
+        return MAX_TRY_COUNT-attemptNumber+1;
+    }
+
+    public int getHiddenNumber() {
+        return hiddenNumber;
     }
 
     public int getAttemptNumber() {
@@ -33,25 +37,6 @@ public class GameMaster {
 
     public void setAttemptNumber(int attemptNumber) {
         this.attemptNumber = attemptNumber;
-    }
-
-    public static String fixNumerical(int num, String... arr) {
-        int preLastDigit = num % 100 / 10;
-        if (preLastDigit == 1) {
-            return String.format("%d %s", num, arr[2]);
-        }
-
-        int lastDigit = num % 10;
-        switch (lastDigit) {
-            case 1:
-                return String.format("%d %s", num, arr[0]);
-            case 2:
-            case 3:
-            case 4:
-                return String.format("%d %s", num, arr[1]);
-            default:
-                return String.format("%d %s", num, arr[2]);
-        }
     }
 }
 
